a0260ccc77ae6240fcbe6867c57a41da9294ef06
Leon24k <rumadirivo@gmail.com>
2023-10-28 04:25:24 -0400

    build new model on models.py CarMake & CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
+import uuid
+import json
 
 
 # Create your models here.
@@ -9,6 +11,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100, default='Make')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "Name: " + self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -20,6 +28,33 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100, default='Car')
+    id = models.IntegerField(default=1, primary_key=True)
+
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Name : " + self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
@@ -47,11 +82,13 @@ class DealerReview:
         self.name = name
         self.purchase = purchase
         self.review = review
-        self.purchase_date = purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
-        self.sentiment = sentiment
+        
+        #Optional 
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+        self.sentiment = ""
         self.id = id
 
     def __str__(self):
